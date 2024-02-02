0625ec083d0fd8cb32ddb4637bc46fb2ee24742b
KING143ND <nvnaveenchaudhary@gmail.com>
2024-02-02 19:48:41 +0530

    bug fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls import re_path as url
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include("App.urls")),
+    # path('', index),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
